1746bc1f2c40d08693f8a53941b5258831e5973a
Hakan Celebi <hakan.celebi@turk.net>
2022-10-29 17:21:17 +0300

    refactor search result mapping

--- src/JiraClient/Mapper/SearchResultMapper.php
+++ src/JiraClient/Mapper/SearchResultMapper.php
@@ -6,14 +6,14 @@ use JiraClient\Dto\SearchResult;
 
 class SearchResultMapper
 {
-    public static function map(\stdClass $issueData) : SearchResult {
+    public static function map(\stdClass $response) : SearchResult {
         $searchResult = new SearchResult();
-        $searchResult->setMaxResult($issueData->maxResult);
-        $searchResult->setStartAt($issueData->startAt);
-        $searchResult->setTotal($issueData->total);
+        $searchResult->setMaxResult($response->maxResult);
+        $searchResult->setStartAt($response->startAt);
+        $searchResult->setTotal($response->total);
         $issues = [];
-        foreach ($issueData as $issueDatum) {
-            $issues[] = IssueMapper::map($issueDatum);
+        foreach ($response->issues as $issueResponse) {
+            $issues[] = IssueMapper::map($issueResponse);
         }
         $searchResult->setIssues($issues);
         return $searchResult;
